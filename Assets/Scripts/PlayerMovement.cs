c170cb709675f8c705f5489dca5ff31e5bb6a9b4
Gerald Col <gnut@gnut.eu>
2022-11-16 16:07:20 +0100

    Lesson 1.4 - Step into the Driver's Seat

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] float movementSpeed = 8f;
-    [SerializeField] float jumpForce = 9f;
+    [SerializeField] float jumpForce = 8f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +20,12 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        // Moves the capsule based on Horizontal and Vertical Input with momement Speed
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
         if (Input.GetButtonDown("Jump"))
         {
+            // Jump the capsule with jumpForce
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
 
